fa008cc39fc3f5f123aa6166a72d382a43f20c1a
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-03-07 22:44:50 -0800

    Added Helper Function to Set Attributes on DOM Elements

--- script.js
+++ script.js
@@ -7,17 +7,28 @@ const count = 10;
 const apiKey = 'kLEhZ2be4Ci7GimZ5DD-kpPrZ3mU_oRlcxJOBWFYYaE';
 const apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key])
+  }
+}
+
 // Create Elements for links and photos add to DOM
 function displayPhotos() {
   photoArr.forEach((photo) => {
     const item = document.createElement('a');
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank"
+    })
 
     const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description
+    });
 
     item.appendChild(img);
     imageContainer.appendChild(item);
