e9e7bb76918e57cbdab52de0f5e92a98a75632b1
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-03-07 22:14:06 -0800

    grabbing random imgs from unsplash api

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello World");
+const count = 10;
+const apiKey = 'kLEhZ2be4Ci7GimZ5DD-kpPrZ3mU_oRlcxJOBWFYYaE';
+const apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+// Get Photos from Unsplash API
+async function getPhotos() {
+  try {
+    const res = await fetch(apiURL);
+    const data = await res.json()
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getPhotos()
