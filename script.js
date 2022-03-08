4f91bd8e2a628f213adb875ec4013fef20b62a54
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-03-07 22:32:16 -0800

    Created Elements for links and photos add to DOM

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById("loader");
+
+let photoArr = []
+
 const count = 10;
 const apiKey = 'kLEhZ2be4Ci7GimZ5DD-kpPrZ3mU_oRlcxJOBWFYYaE';
 const apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+// Create Elements for links and photos add to DOM
+function displayPhotos() {
+  photoArr.forEach((photo) => {
+    const item = document.createElement('a');
+    item.setAttribute('href', photo.links.html);
+    item.setAttribute('target', '_blank');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', photo.urls.regular);
+    img.setAttribute('alt', photo.alt_description);
+    img.setAttribute('title', photo.alt_description);
+
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
 
 // Get Photos from Unsplash API
 async function getPhotos() {
   try {
     const res = await fetch(apiURL);
-    const data = await res.json()
-    console.log(data);
+    photoArr = await res.json()
+    displayPhotos()
   } catch (error) {
     console.log(error);
   }
